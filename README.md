0583f8ec7c2993f68bf3ffb0114dbc02112b63a8
ldaijiw <lwaltmann@spartaglobal>
2020-11-09 11:15:30 +0000

    math floor/ceil task

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Python Modules
 
-- Built in functions
+- Built-in functions
 - Python library
 - What is pip and how do we use it
 - APIs with python
+
+## Built-in Functions
+- Built in functions help us accelerate our development of software
+
+**Task Using Math**
+- get user input of a float number
+- check if the number is lower than .50 then round the figure to lower end
+- check if the number is greater than .51 then round the figure to higher end
+- example - num_float = 23.66 - round it to 24, num_float = 23.50 - round it to lower end
+- tips - get help online - python library
---
+++ modules.py
@@ -0,0 +1,40 @@
+# Examples of built in functions
+
+# import is keyword used to call modules from Python library
+
+import random
+import math
+
+# generates a float number from 0 to 1
+print(x := random.random())
+print(round(x))
+
+
+# using math.floor to round a number down
+num_float = 23.44
+print(num_float)
+print(math.floor(num_float))
+
+# using math.ceil to round a number up
+print(math.ceil(num_float))
+
+
+# Task
+# get user input of a float number
+# check if the number is lower than .50 then round the figure to lower end
+# check if the number is greater than .51 then round the figure to higher end
+# example - num_float = 23.66 - round it to 24, num_float = 23.50 - round it to lower end
+# tips - get help online - python library
+
+def round_num():
+    num = float(input("What number would you like to round?\n"))
+
+    # simpler method of rounding number
+    # return round(num)
+
+    if num - math.floor(num) >= 0.5:
+        return math.ceil(num)
+    return math.floor(num)
+
+
+print(round_num())
